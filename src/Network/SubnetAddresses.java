b609192d56a4368ba4ef64376b9d1c3c2afe9efd
AjayVyas68 <ajayvyas625@gmail.com>
2022-12-02 18:21:10 +0530

    upload in github

--- src/Network/SubnetAddresses.java
+++ src/Network/SubnetAddresses.java
@@ -1,24 +1,26 @@
 package Network;
 
-import java.net.*;
-import java.util.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 public class SubnetAddresses {
 
     public static void main(String[] args) throws UnknownHostException, SocketException {
         // get all possible IP addresses of all LANs
         for (Enumeration<NetworkInterface> ifaces =
-                NetworkInterface.getNetworkInterfaces();
-        ifaces.hasMoreElements(); )
-        {
+             NetworkInterface.getNetworkInterfaces();
+             ifaces.hasMoreElements(); ) {
             NetworkInterface iface = ifaces.nextElement();
             System.out.println(iface.getName() + ":");
             for (Enumeration<InetAddress> addresses =
                  iface.getInetAddresses();
-                 addresses.hasMoreElements(); )
-            {
+                 addresses.hasMoreElements(); ) {
                 InetAddress address = addresses.nextElement();
                 System.out.println(" add " + address);
+                System.out.println("upload in github");
             }
         }
 
